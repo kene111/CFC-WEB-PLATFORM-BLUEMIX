fd43f7dfc7bd8eb55324b1e836b0f78305810695
Kenechi Ojukwu <kenechiojukwu@gmail.com>
2020-07-30 08:26:17 +0100

    web platform

---
+++ scraper/urls.py
@@ -0,0 +1,11 @@
+from django.urls import path
+from . import views
+#from . views import ScraperListView
+
+
+urlpatterns = [
+    path('infomation/', views.scrape, name = 'scraper-scraped'),
+    path('twitter_infomation/', views.tweet, name = 'scraper-tweets'),
+    path('prediction/',  views.prediction, name = 'scraper-prediction'),
+    path('maps/', views.map, name = 'scraper-maps'),    
+]
