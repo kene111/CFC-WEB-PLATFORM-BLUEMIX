a9d34d1ef84210777fccea346025b14d8b621d27
Kenechi Ojukwu <kenechiojukwu@gmail.com>
2020-08-06 12:06:42 +0100

    minor update

--- CODE/settings.py
+++ CODE/settings.py
@@ -156,7 +156,7 @@ EMAIL_BACKEND = 'django.core.mail.backends.smtp.EmailBackend'
 EMAIL_HOST = 'smtp.gmail.com'
 EMAIL_PORT = 587
 EMAIL_HOST_USER = 'tocrear.3@gmail.com'
-EMAIL_HOST_PASSWORD = '3L_9sy_Congr00'
+EMAIL_HOST_PASSWORD = ''
 EMAIL_USE_TLS = True
 EMAIL_USE_SSL = False
 
